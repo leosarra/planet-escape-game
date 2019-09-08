e2ba6920eb94e150ede56288c16b965c3c4e16d3
Leonardo Sarra <leonardosarra@outlook.com>
2019-09-08 17:34:40 +0200

    improve performance of checkCollisions()

--- js/game/projectiles.js
+++ js/game/projectiles.js
@@ -1,33 +1,36 @@
 Projectile = function (holder) {
     var geom, mat;
     this.holder = holder;
-	var geom = new THREE.OctahedronGeometry(5, 0);
-	var mat = new THREE.MeshPhongMaterial({
-		color: 0x38761D,
-		shininess: 0,
-		specular: 0xffffff,
-		flatShading: true
-	});
-	this.mesh = new THREE.Mesh(geom, mat);
-	this.mesh.castShadow = true;
+    var geom = new THREE.BoxGeometry(5, 2, 2);
+    var mat = new THREE.MeshPhongMaterial({
+        color: 0x7CFC00,
+        shininess: 0,
+        specular: 0xffffff,
+        flatShading: true,
+        opacity: 0.0,
+        transparent: true,
+    });
+    this.mesh = new THREE.Mesh(geom, mat);
 }
 
 Projectile.prototype.fire = function (pos) {
     var _this = this;
     var _p = this.mesh.parent;
     var speed = 0.6;
-    var targetX = pos.x + 400;
+    var targetX = pos.x + 2000;
     this.holder.activeProjectiles.unshift(this);
-    TweenMax.to(this.mesh.position, speed, {x: targetX, y: pos.y, ease: Linear.easeOut, onComplete: function () {
-          if (_p) _p.remove(_this.mesh);
-          for (var i = 0; i < _this.holder.activeProjectiles.length; i++) {
-              if (_this.holder.activeProjectiles[i] == _this) {
-                _this.holder.projectilesPool.unshift(_this.holder.activeProjectiles.splice(i, 1)[0]);
-                break;
-              }
-          }
+    this.tween1 = TweenMax.to(this.mesh.material, 0.05, { opacity: 1 });
+    this.tween2 = TweenMax.to(this.mesh.position, speed, {
+        x: targetX, y: pos.y, ease: Linear.easeOut, onComplete: function () {
+            if (_p) _p.remove(_this.mesh);
+            for (var i = 0; i < _this.holder.activeProjectiles.length; i++) {
+                if (_this.holder.activeProjectiles[i] == _this) {
+                    _this.holder.projectilesPool.unshift(_this.holder.activeProjectiles.splice(i, 1)[0]);
+                    break;
+                }
+            }
         }
-      });
+    }).duration(5);
 }
 
 ProjectilesHolder = function (nProjectiles) {
@@ -35,18 +38,18 @@ ProjectilesHolder = function (nProjectiles) {
     this.projectilesPool = [];
     this.activeProjectiles = [];
     for (var i = 0; i < nProjectiles; i++) {
-		var projectile = new Projectile(this);
-		this.projectilesPool.push(projectile);
-	}
+        var projectile = new Projectile(this);
+        this.projectilesPool.push(projectile);
+    }
 }
 
 ProjectilesHolder.prototype.spawnParticles = function (pos) {
 
     if (this.projectilesPool.length) {
         projectile = this.projectilesPool.pop();
-        projectile.mesh.visible = true;
+        projectile.mesh.material.opacity = 0;
     } else {
-        projectile = new Projectile(data);
+        projectile = new Projectile(this);
     }
     this.mesh.add(projectile.mesh);
     projectile.mesh.position.y = pos.y;
@@ -57,15 +60,14 @@ ProjectilesHolder.prototype.spawnParticles = function (pos) {
 ProjectilesHolder.prototype.checkCollisions = function (pos) {
     for (var i = 0; i < this.activeProjectiles.length; i++) {
         var projectile = this.activeProjectiles[i];
-        if (!projectile.mesh.visible) {
-            console.log("IGNORED");
-            continue;
-        }
         var diffPos = projectile.mesh.position.clone().sub(pos.clone());
         var d = diffPos.length();
-        console.log(d);
-		if (d < 15) {
+        if (d < 18) {
             projectile.mesh.visible = false;
+            projectile.tween1.kill();
+            projectile.tween2.kill();
+            this.projectilesPool.unshift(this.activeProjectiles.splice(i, 1)[0]);
+            i--;
             return true;
         }
     }
