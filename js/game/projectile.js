47155a1b4cb31120242f8a2724b56d2435255861
Leonardo Sarra <leonardosarra@outlook.com>
2019-09-18 02:13:43 +0200

    Reword distance computation for projectiles

--- js/game/projectile.js
+++ js/game/projectile.js
@@ -61,9 +61,8 @@ ProjectilesHolder.prototype.checkCollisions = function (pos) {
     var ret = false;
     for (var i = 0; i < this.activeProjectiles.length; i++) {
         var projectile = this.activeProjectiles[i];
-        var diffPos = projectile.mesh.position.clone().sub(pos.clone());
-        var d = diffPos.length();
-        if (d < 18) {
+        var d = projectile.mesh.position.distanceTo(pos);
+        if (d < 14) {
             projectile.tween1.kill();
             projectile.tween2.kill();
             this.projectilesPool.unshift(this.activeProjectiles.splice(i, 1)[0]);
