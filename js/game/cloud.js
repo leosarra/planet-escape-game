9e8c436b0367c6ff3f40428e78c49a0eefae871b
Leonardo Sarra <leonardosarra@outlook.com>
2019-09-21 19:15:16 +0200

    Refactoring for cloud and sky

--- js/game/cloud.js
+++ js/game/cloud.js
@@ -6,28 +6,28 @@ Cloud = function () {
   var mat = new THREE.MeshPhongMaterial({
     color: Colors.white,
   });
-  var nBlocs = 3 + Math.floor(Math.random() * 3);
-  for (var i = 0; i < nBlocs; i++) {
-    var m = new THREE.Mesh(geom.clone(), mat);
-    m.position.x = i * 15;
-    m.position.y = Math.random() * 10;
-    m.position.z = Math.random() * 10;
-    m.rotation.z = Math.random() * Math.PI * 2;
-    m.rotation.y = Math.random() * Math.PI * 2;
-    var s = .1 + Math.random() * .8;
-    m.scale.set(s, s, s);
-    m.castShadow = true;
-    m.receiveShadow = true;
-    this.mesh.add(m);
+  var nChildren = 3 + Math.floor(Math.random() * 3);
+  for (var i = 0; i < nChildren; i++) {
+    var childrenMesh = new THREE.Mesh(geom.clone(), mat);
+    childrenMesh.position.x = i * 15;
+    childrenMesh.position.y = Math.random() * 10;
+    childrenMesh.position.z = Math.random() * 10;
+    childrenMesh.rotation.z = Math.random() * Math.PI * 2;
+    childrenMesh.rotation.y = Math.random() * Math.PI * 2;
+    var scaleFactor = .1 + Math.random() * .8;
+    childrenMesh.scale.set(scaleFactor, scaleFactor, scaleFactor);
+    childrenMesh.castShadow = true;
+    childrenMesh.receiveShadow = true;
+    this.mesh.add(childrenMesh);
   }
 }
 
 Cloud.prototype.rotate = function () {
   var l = this.mesh.children.length;
   for (var i = 0; i < l; i++) {
-    var m = this.mesh.children[i];
+    var childrenMesh = this.mesh.children[i];
     var animationBoost = (i + 1) * 0.5;
-    m.rotation.z += Math.random() * .0055 * animationBoost;
-    m.rotation.y += Math.random() * .00225 * animationBoost;
+    childrenMesh.rotation.z += Math.random() * .0055 * animationBoost;
+    childrenMesh.rotation.y += Math.random() * .00225 * animationBoost;
   }
 }
--- js/game/sky.js
+++ js/game/sky.js
@@ -5,24 +5,24 @@ Sky = function (game, nClouds) {
 	this.clouds = [];
 	var stepAngle = Math.PI * 2 / this.nClouds;
 	for (var i = 0; i < this.nClouds; i++) {
-		var c = new Cloud();
-		this.clouds.push(c);
+		var cloud = new Cloud();
+		this.clouds.push(cloud);
 		var a = stepAngle * i;
 		var h = 750 + Math.random() * 200;
-		c.mesh.position.y = Math.sin(a) * h;
-		c.mesh.position.x = Math.cos(a) * h;
-		c.mesh.position.z = -400 - Math.random() * 500;
-		c.mesh.rotation.z = a + Math.PI / 2;
-		var s = 1 + Math.random() * 2;
-		c.mesh.scale.set(s, s, s);
-		this.mesh.add(c.mesh);
+		cloud.mesh.position.y = Math.sin(a) * h;
+		cloud.mesh.position.x = Math.cos(a) * h;
+		cloud.mesh.position.z = -400 - Math.random() * 500;
+		cloud.mesh.rotation.z = a + Math.PI / 2;
+		var scaleFactor = 1 + Math.random() * 2;
+		cloud.mesh.scale.set(scaleFactor, scaleFactor, scaleFactor);
+		this.mesh.add(cloud.mesh);
 	}
 }
 
 Sky.prototype.moveClouds = function () {
 	for (var i = 0; i < this.nClouds; i++) {
-		var c = this.clouds[i];
-		c.rotate();
+		var cloud = this.clouds[i];
+		cloud.rotate();
 	}
 	this.mesh.rotation.z += this.game.baseSpeed * this.game.deltaTime;
 
